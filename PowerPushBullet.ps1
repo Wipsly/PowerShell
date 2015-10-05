afa04a947fac09b99fd26d9d253266d1bea4fbbd
Wipsly <wipsly@outlook.de>
2015-10-05 16:26:59 +0200

    Added PowerPushBullet

---
+++ PowerPushBullet.ps1
@@ -0,0 +1,31 @@
+﻿#
+# PowerPushBullet created by Wipsly - www.wipsly.com
+#
+
+# Function to send push notification
+function sendPushNotification($title, $message) {
+
+	# API-Key
+	$apikey = 'YOURAPIKEY'
+
+    # Make PSCredential object from apikey
+    $cred = New-Object System.Management.Automation.PSCredential ($apiKey, (ConvertTo-SecureString $apikey -AsPlainText -Force))
+
+    # Get all devices
+    $devices = Invoke-RestMethod -Uri 'https://api.pushbullet.com/api/devices' -Method Get -Credential $cred
+
+    # Loop trough all devices and send notification
+    foreach ($device in $devices.devices) {
+
+        # Create the notification
+        $notification = @{
+            device_iden = $device.iden
+            type = 'note'
+            title = $title
+            body = $message
+        }
+
+        # Send the notification
+        Invoke-RestMethod -Uri 'https://api.pushbullet.com/api/pushes' -Body $notification -Method Post -Credential $cred
+    }
+}
